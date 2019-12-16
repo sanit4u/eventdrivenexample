55b259678f75510391f08cd22fda14374afecb53
Santosh Kumar Rajaguru <sanit4u@gmail.com>
2019-12-16 16:22:35 +0100

    initial creation of config server

---
+++ config-server/src/test/java/de/sanit4u/evd/example/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sanit4u.evd.example.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
