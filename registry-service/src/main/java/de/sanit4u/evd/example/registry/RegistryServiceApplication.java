3cf4487845c9bac29eecc6aa4cae57ff6cbbc945
Santosh Kumar Rajaguru <sanit4u@gmail.com>
2019-12-16 16:35:21 +0100

    registry service initial commit

---
+++ registry-service/src/main/java/de/sanit4u/evd/example/registry/RegistryServiceApplication.java
@@ -0,0 +1,15 @@
+package de.sanit4u.evd.example.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
---
+++ registry-service/src/test/java/de/sanit4u/evd/example/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sanit4u.evd.example.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
